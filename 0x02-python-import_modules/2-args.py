fc527965029ca54be2fbf97a6720de8c52600c4f
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-03-30 17:00:22 +0200

    Task-2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+script_name = sys.argv[0]
+arguments = sys.argv[1:]
+if len(sys.argv) == 1:
+    print("0 arguments.")
+elif len(sys.argv) == 2:
+    print(f"{len(sys.argv) - 1}", "argument:")
+    for index in (sys.argv[1:]):
+        print(f"{len(sys.argv) - 1}", sys.argv[1])
+else:
+    print(f"{len(sys.argv) - 1} arguments:")
+    for index, arg in enumerate(arguments, 1):
+        print(f"{index}: {arg}")
