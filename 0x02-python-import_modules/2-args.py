5d37e86e682aa3e0ab46d77017fe1e9afe3bbb96
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-03-30 17:06:49 +0200

    Task-2, Check the pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 import sys
 if __name__ == "__main__":
     script_name = sys.argv[0]
-arguments = sys.argv[1:]
-if len(sys.argv) == 1:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print(f"{len(sys.argv) - 1}", "argument:")
-    for index in (sys.argv[1:]):
-        print(f"{len(sys.argv) - 1}:", sys.argv[1])
-else:
-    print(f"{len(sys.argv) - 1} arguments:")
-    for index, arg in enumerate(arguments, 1):
-        print(f"{index}: {arg}")
+    arguments = sys.argv[1:]
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print(f"{len(sys.argv) - 1}", "argument:")
+        for index in (sys.argv[1:]):
+            print(f"{len(sys.argv) - 1}:", sys.argv[1])
+    else:
+        print(f"{len(sys.argv) - 1} arguments:")
+        for index, arg in enumerate(arguments, 1):
+            print(f"{index}: {arg}")
