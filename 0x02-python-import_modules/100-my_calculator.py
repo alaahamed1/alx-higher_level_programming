e25ea27b1db887691f03438bfb01178288540e9d
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-03-31 01:21:59 +0200

    Task-6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, div, mul, sub
+    import sys
+    arguments = sys.argv[1:]
+
+    if len(arguments) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    operator = {'+': add, '-': sub, '*': mul, '/': div}
+    if sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    res = operator[sys.argv[2]](int(sys.argv[1]),int(sys.argv[3]))
+    print(int(sys.argv[1]), sys.argv[2], int(sys.argv[3]), "=", res)
