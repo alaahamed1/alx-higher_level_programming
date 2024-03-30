ba6622c2be9b4aa3b43244811f499721ab6fe327
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-03-30 16:00:57 +0200

    Task-0 & README file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__":
-    a = 10
-    b = 20
+    a = 1
+    b = 2
     result = int("{:d}".format(add(a, b)))
     print(f"{a} + {b} = {result}")
