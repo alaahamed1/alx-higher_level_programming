041fa48b566b820dac2b7eec911b65f36ff562d8
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-04 18:00:05 +0200

    Task-1 in Python - Exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,5 @@ def safe_print_integer(value):
         if isinstance(value, int):
             print(value)
             return True
-        else:
-            return False
     except:
-        print()
+        return False
