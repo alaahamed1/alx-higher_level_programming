9665a2bf6777b6d9b70ede50a7107500e2c0a1f0
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-15 02:46:03 +0200

    Task-2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new_list = my_list[:]
+    cur_idx = 0
+    for i in my_list:
+        if idx == cur_idx:
+            i = element
+            new_list[idx] = element
+            return new_list
+        cur_idx += 1
