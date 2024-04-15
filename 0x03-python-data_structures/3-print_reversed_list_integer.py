442c31c217e42c234c415d8d6fe165cc0f0cf75c
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-15 03:14:36 +0200

    Task3 in 0x03.Python-Data Structures: Lists, Tuples project

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new = list(range(len(my_list)))
+    if my_list is None:
+        return
+    else:
+        new = list(range(len(my_list)))
     new.reverse()
     for i in new:
         print("{:d}".format(my_list[i]))
