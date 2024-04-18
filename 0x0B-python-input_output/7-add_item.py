b1da3eeaea01dbf5e6e151228dd6e264078ee4b1
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-19 01:00:18 +0200

    Task-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,11 @@ If the file doesn’t exist, it should be created'''
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-if __name__ == "__main__":
-    argument = sys.argv[1:]
-    save_to_json_file(argument, "add_item.json")
-    load_from_json_file("add_item.json")
+
+try:
+    item = load_from_json_file("add_item.json")
+except Exception:
+    item = []
+for arg in sys.argv[1:]:
+    item.append(arg)
+save_to_json_file(item, "add_item.json")
