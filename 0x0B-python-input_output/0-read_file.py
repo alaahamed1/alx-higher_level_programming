01605402d388b87c293da16cf1fe952549848af9
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-18 19:50:02 +0200

    Task-0 in 0x0B. Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as file:
+        '''reads a text file (UTF8) and prints it to stdout'''
         for line in file:
             print(line, end='')
