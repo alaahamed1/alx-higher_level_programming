0371fd108c91ad65b2acc1449430c071451e3f4d
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-19 00:24:06 +0200

    Task-7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+'''adds all arguments to a Python list, and then save them to a file
+the file named add_item.json
+If the file doesn’t exist, it should be created'''
+
+
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-import sys
 if __name__ == "__main__":
     argument = sys.argv[1:]
     print(argument)
