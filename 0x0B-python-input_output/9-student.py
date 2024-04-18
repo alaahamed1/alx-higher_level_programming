2934f9fbb7b99699ffa18590b38b818b5f70ff59
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-19 01:51:40 +0200

    Task-9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Module of only one class Student'''
+
+
+class Student:
+    '''Defines a student by:
+    Public instance attributes:
+        first_name, last_name, age
+    '''
+    def __init__(self, first_name, last_name, age):
+        '''Instantiation with first_name, last_name and age'''
+        self.first_name = first_name,
+        self.last_name = last_name,
+        self.age = age
+        
+    def to_json(self):
+        '''retrieves a dictionary representation of a Student instance'''
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
