27d6444f296a129b07207acaf422e9d81a3c21c9
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-18 19:53:50 +0200

    Task-0 in 0x0B. Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-'''Module of only one function read_file(filename="")'''
-
+'''Module of a function read_file(filename="")'''
 
 def read_file(filename=""):
     '''reads a text file (UTF8) and prints it to stdout'''
-    with open(filename, encoding="utf-8") as afile:
-        print(afile.read(), end="")
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
