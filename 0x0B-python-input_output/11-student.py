19835aa1880562c4029bf1e5e506593aa42b2072
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-19 23:07:56 +0200

    some updates

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -9,8 +9,8 @@ class Student:
     '''
     def __init__(self, first_name, last_name, age):
         '''Instantiation with first_name, last_name and age'''
-        self.first_name = first_name,
-        self.last_name = last_name,
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
     def to_json(self, attrs=None):
         '''retrieves a dictionary representation of a Student instance
