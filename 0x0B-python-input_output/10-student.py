0df1f115d5a69093aa1438bffa74d814cb455ed7
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-19 23:05:39 +0200

    some updates

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -9,10 +9,10 @@ class Student:
     '''
     def __init__(self, first_name, last_name, age):
         '''Instantiation with first_name, last_name and age'''
-        self.first_name = first_name,
-        self.last_name = last_name,
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
         '''retrieves a dictionary representation of a Student instance
         If attrs is a list of strings,only attribute names contained
