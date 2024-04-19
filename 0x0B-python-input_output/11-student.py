fa798dcff3ef70361fa6f08306263825203f76dc
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-19 23:09:05 +0200

    solving some pycodestyle issues

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -12,6 +12,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self, attrs=None):
         '''retrieves a dictionary representation of a Student instance
         If attrs is a list of strings,only attribute names contained
