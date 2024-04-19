24ab308d20e71a7cddc377ee0212eef0a76f04ba
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-19 05:53:57 +0200

    Task-11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -13,7 +13,10 @@ class Student:
         self.last_name = last_name,
         self.age = age
     def to_json(self, attrs=None):
-        
+        '''retrieves a dictionary representation of a Student instance
+        If attrs is a list of strings,only attribute names contained
+        in this list must be retrieved.Otherwise,
+        all attributes must be retrieved'''
         if attrs is None:
             return self.__dict__
         else:
