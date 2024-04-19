9b62378783f8f4090cebc9d421858135c4d759fa
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-19 05:48:35 +0200

    Task-11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -12,13 +12,13 @@ class Student:
         self.first_name = first_name,
         self.last_name = last_name,
         self.age = age
-        
     def to_json(self, attrs=None):
-        
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {attr: getattr(self, attr) 
+                    for attr in attrs if hasattr(self, attr)}
+
     def reload_from_json(self, json):
         '''replaces all attributes of the Student instance:
         You can assume json will always be a dictionary'''
