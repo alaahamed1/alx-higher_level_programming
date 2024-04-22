29e79b0b8bc67dd629ba7435c0fa6f8dd0cbd11a
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-23 01:38:59 +0200

    modifing pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+'''class Square that defines a square'''
+
+
 class Square:
+    '''empty class'''
     pass
     def __init__(self, _size):
+        '''Private instance attribute: size'''
         self._size = _size
