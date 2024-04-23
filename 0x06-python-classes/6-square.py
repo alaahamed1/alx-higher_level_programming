9dd1d07d418d7963282f6603895db8c10fd817ce
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-23 06:31:54 +0200

    Task-6 test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -52,9 +52,9 @@ class Square():
     def position(self, value):
         try:
             if (isinstance(value, tuple) and
-                    len(value) == 2 and
-                    value[0] >= 0 and
-                    value[1] >= 0):
+                len(value) == 2 and
+                value[0] >= 0 and
+                value[1] >= 0):
                 self.__position = value
             else:
                 raise TypeError
