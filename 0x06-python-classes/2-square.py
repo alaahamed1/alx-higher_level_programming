a28b884e2371447741e0c00a848f5c9e4ff0632f
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-23 02:00:13 +0200

    Task-2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Just class `Square` that defines a square'''
+
+
+class Square:
+    '''class with Private instance attribute: size'''
+    pass
+
+    def __init__(self, size = 0):
+        '''Instantiation with size optional
+        Args:
+            size (int): the size of the aquare, must be >= 0'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
