a7a7b4065f39ed210be0fd33f36840a8a0066244
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-23 06:21:28 +0200

    Task-6 test

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -2,45 +2,35 @@
 '''Just class `Square` that defines a square'''
 
 
-class Square:
-    '''In this class
-    Private instance attribute: size
-    '''
+class Square():
+    '''class with Private instance attribute: size'''
 
     def __init__(self, size=0, position=(0, 0)):
         ''' Instantiation with size optional
         Args:
-            size (int): the size of the aquare, must be >= 0
-            position (tuple): a tuple of 2 positive integers
-        '''
-        self.size = size
-        self.position = position
+            size (int): the size of the aquare, must be >= 0'''
+        self.__size = size
+        self.__position = position
 
     def area(self):
-        '''Get the area of the square
-
-        Returns:
-            the area of the square
-        '''
+        """Return the current square area"""
         return self.__size ** 2
 
     def my_print(self):
-        '''Print the area of the square object with #s'''
-        if self.__size == 0:
+        if self.size == 0:
             print()
         else:
-            for j in range(self.__position[1]):
+            for _ in range(self.__position[1]):
                 print()
-            for i in range(self.__size):
+            for _ in range(self.__size):
                 if self.__position[0] > 0:
                     print(" " * self.__position[0], end="")
                 print("#" * self.__size)
 
     @property
     def size(self):
-        '''the getter method of class Square
-        the size of the aquare, must be >= 0 and of course int type
-        '''
+        '''getter method of class Square the size of the aquare,
+        must be >= 0 and of course int type'''
         return self.__size
 
     @size.setter
@@ -60,14 +50,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        try:
-            if (isinstance(value, tuple) and
-                    len(value) == 2 and
-                    value[0] >= 0 and
-                    value[1] >= 0):
-                self.__position = value
-            else:
-                raise TypeError
-        except Exception:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        
+        if (not isinstance(value, tuple) and len(value) != 2
+        and not all(isinstance(val, int) for val in value)and
+        any(val < 0 for val in value)
+        ):
+            raise ValueError('position must be a tuple of 2 positive integers')
+        self.__position = value
