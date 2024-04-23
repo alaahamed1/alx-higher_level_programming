85955ffabd6cbd2e8af2a3949ca9792934e15d87
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-23 04:02:47 +0200

    0x0C. Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''class base'''
+
+
+class Base:
+    '''private class attribute __nb_objects = 0'''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
