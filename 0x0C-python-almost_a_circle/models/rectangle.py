4035941f12b13c2b6e660b17091060b62b70fa0d
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-23 04:23:17 +0200

    0x0C. Python - Almost a circle Task-2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    
+    '''class Rectangle'''
     def __init__(self, width, height, x=0, y=0, id=None):
         '''constructor'''
         super.__init__(id)
