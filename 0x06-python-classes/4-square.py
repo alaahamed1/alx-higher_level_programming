bb349b8d5cfb0bcee8f769edb0c08840d03e2dc3
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-04-23 02:27:14 +0200

    Task-4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Just class `Square` that defines a square'''
+
+
+class Square():
+    '''class with Private instance attribute: size'''
+    pass
+
+    def __init__(self, size=0):
+        ''' Instantiation with size optional
+        Args:
+            size (int): the size of the aquare, must be >= 0'''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''getter method of class Square the size of the aquare,
+        must be >= 0 and of course int type'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''Get the area of the square
+        Returns: the area of the square'''
+        return (self.__size ** 2)
