058882d02cd6750d9060994c2adbb88a9c9a40cd
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-19 02:35:05 +0300

    Task 10 in /0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const arg = parseInt(process.argv[2]);
+  function factorial(n) {
+    if (!n || n === 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
+console.log(factorial(arg));
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 function add (a, b) {
-  const result = a + b;
-  return result;
+  const res = a + b;
+  return res;
 }
 console.log(add(num1, num2));
