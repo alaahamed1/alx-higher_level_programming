614052046f64d67dba8e91b96c1295d6ebfdf289
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-19 02:20:50 +0300

    Task 9 in /0x12-javascript-warm_up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const firstArgv = parseInt(process.argv[2])
-const secondArgv = parseInt(process.argv[3])
-function add(a, b) {
-  a = firstArgv;
-  b = secondArgv;
-  const res = a + b;
-  return res;
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
+function add (a, b) {
+  const result = a + b;
+  return result;
 }
 console.log(add(num1, num2));
