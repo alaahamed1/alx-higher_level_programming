712ff6f660c422351e8089e1d6780b31472fa66a
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-19 02:05:18 +0300

    Task 8 in /0x12-javascript-warm_up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const argv = parseInt(process.argv[2]);
 if (isNaN(argv)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let i =0; i < argv; i++){
-        let row = "";
-        for (let j = 0; j < argv; j++){
-            row += "X";
-        }
-        console.log(row);
+  for (let i = 0; i < argv; i++) {
+    let row = "";
+    for (let j = 0; j < argv; j++) {
+      row += "X";
     }
+    console.log(row);
+  }
 }
