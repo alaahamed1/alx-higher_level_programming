37c5ce04c4563afa5e8331b04c61689d7f6b92e6
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-19 02:36:49 +0300

    Task 10 in /0x12-javascript-warm_up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const arg = parseInt(process.argv[2]);
-  function factorial(n) {
-    if (!n || n === 1) {
-      return 1;
-    } else {
-      return n * factorial(n - 1);
-    }
+const n = parseInt(process.argv[2]);
+function factorial (a) {
+  if (!a || a === 1) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
   }
-console.log(factorial(arg));
+}
+console.log(factorial(n));
