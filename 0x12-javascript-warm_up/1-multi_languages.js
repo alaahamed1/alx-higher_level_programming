ea539e35e65db5da5b6577159c97ce787e3d85b3
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-18 23:51:26 +0300

    Task1 in 0x12-javascript-warm_up project and modified README.md

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
-let a = 'C is fun';
-let b = 'Python is cool';
-let c = 'JavaScript is amazing';
-console.log(a)
-console.log(b)
-console.log(c)
+let str = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(str)
