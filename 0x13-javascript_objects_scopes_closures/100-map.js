09f59beabf47095cfc6bc9dd545df4eeabe15f71
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-06-20 23:10:20 +0300

    Advanced Task-11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
+
 const list = require('./100-data.js').list;
-const mylist = list.map((num, index) => num * index);
+
+const newList = list.map((num, index) => num * index);
+
 console.log(list);
-console.log(mylist);
+console.log(newList);
