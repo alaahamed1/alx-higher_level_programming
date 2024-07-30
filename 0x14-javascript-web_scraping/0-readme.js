28cebb91f9bcbe6c59d3ba3697451f02f68b16b6
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-07-30 23:00:16 +0300

    Task-1

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const files = require('files');
+
+files.readFile(process.argv[2], 'utf8', function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+	}
+});
